64f618a944c31888459c87c4105281ab838d523a
angeisa97 <ange.isabel@hotmail.com>
2019-11-18 13:37:17 +0100

    subido prueba-idm.txt

--- Bisiestos/Bisiestos.java
+++ Bisiestos/Bisiestos.java
@@ -1,12 +1,12 @@
 public class Bisiestos {
 
-  // Devuelve true si año es bisiesto, false en caso contrario
-  // Eleva la excepcion InvalidParameter si año no es un parametro valido
+  // Devuelve true si year es bisiesto, false en caso contrario
+  // Eleva la excepcion InvalidParameter si year no es un parametro valido
 
   public static boolean esBisiesto(int year) throws InvalidParameter {
 
     if (year <= 0){
-      throw new InvalidParameter("Año inválido");
+      throw new InvalidParameter("Invalid year");
     }
 
     if ((year %4 == 0 && year %100 != 0) || (year %4 == 0 && year%100 == 0 && year%400 == 0)){
--- Bisiestos/BisiestosTest.java
+++ Bisiestos/BisiestosTest.java
@@ -4,7 +4,12 @@ import java.util.*;
 
 public class BisiestosTest
 {
+     //Bisiestos Bisiestos = new Bisiestos();
+     //private int year;
+
    // no es divisible entre 4
+   // prueba c1.by, c2.bx, c3.bx
+   // se queda en la condicion c1.by
    @Test public void noBisiesto()
    {
       int year = 1997;
@@ -12,6 +17,7 @@ public class BisiestosTest
    }
 
    //divisible entre 4 y entre 100
+   // prueba c1.by, c2.by,c3.bx
    @Test public void noBisiesto2()
    {
       int year = 3000;
@@ -19,27 +25,32 @@ public class BisiestosTest
    }
 
    //divisible entre 4, entre 100 pero no entre 400
+   //prueba c1.bx, c2.by, c3.bx
    @Test public void noBisiesto3()
    {
       int year = 1900;
       assertEquals("No bisiesto tercera condicion", false , Bisiestos.esBisiesto(year));
    }
 
+   //divisible entre 4, entre 100 y entre 400
+   // pueba c1.bx, c2.by, c3.by
    @Test public void itis()
    {
       int year = 2012;
-      assertEquals("No bisiesto", true , Bisiestos.esBisiesto(year));
+      assertEquals("Es bisiesto", true , Bisiestos.esBisiesto(year));
    }
 
+   //divisible entre 4, entre 100 y entre 400
+   // pueba c1.bx, c2.by, c3.by
    @Test public void SiBisiesto()
    {
       int year = 2000;
-      assertEquals("No bisiesto", true , Bisiestos.esBisiesto(year));
+      assertEquals("Esbisiesto", true , Bisiestos.esBisiesto(year));
    }
 
-
+   //si el formato no es valido
    @Test(expected = InvalidParameter.class)
-   public void añoinvalido()
+   public void invalidyear()
    {
      int year = -1; // false
      Bisiestos.esBisiesto(year);
