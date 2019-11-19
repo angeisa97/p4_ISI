b281b06f672f170045da17a6340d5b0602a77624
agallegoroz <rozalengallego@gmail.com>
2019-11-19 17:03:00 +0100

    anado roman y embotelladora

---
+++ embotelladora/embotelladoraTest.java
@@ -0,0 +1,59 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+import java.util.*;
+
+public class embotelladoraTest
+{
+   
+   embotelladora embotelladora = new embotelladora();
+   
+   @Test public void botellasPeq () throws NoSolution
+   {
+      int peq = 5;
+      int big = 3;
+      int total = 20;
+      assertEquals("Numero suficiente de botellas pequenas", 5, embotelladora.calculaBotellasPequenas(peq, big, total));
+   }
+   
+    @Test public void botellasSoloGrandes() throws NoSolution
+   {
+      int peq = 5;
+      int big = 4;
+      int total = 20;
+      assertEquals("Todo cabe en las grandes", 0, embotelladora.calculaBotellasPequenas(peq, big, total));
+   }
+   
+     @Test public void noSuficientesPequenas() throws NoSolution
+   {
+      int peq = 1;
+      int big = 3;
+      int total = 20;
+      assertEquals("No hay botellas pequenas suficientes", -1, embotelladora.calculaBotellasPequenas(peq, big, total));
+   }
+   
+     @Test public void sobraGrande() throws NoSolution
+   {
+      int peq = 4;
+      int big = 1;
+      int total = 4;
+      assertEquals("Sobran litro con la botella grande", 4, embotelladora.calculaBotellasPequenas(peq, big, total));
+   }
+   
+    @Test public void botellasPeq2() throws NoSolution
+   {
+      int peq = 2;
+      int big = 3;
+      int total = 12;
+      assertEquals("Numero suficiente de botellas pequenas", 2, embotelladora.calculaBotellasPequenas(peq, big, total));
+   }
+   
+   @Test public void botellasPeq3() throws NoSolution
+   {
+      int peq = 3;
+      int big = 10;
+      int total = 43;
+      assertEquals("Numero suficiente de botellas pequenas", 3, embotelladora.calculaBotellasPequenas(peq, big, total));
+   }
+}
+
+
---
+++ romanNumeral/romanNumeral.java
@@ -0,0 +1,61 @@
+
+
+
+public class romanNumeral {
+	
+    public int convierte(String s) throws InvalidParameter{
+        int decimal = 0;
+        int lastNumber = 0;
+        String romanNumeral = s.toUpperCase();
+        for (int x = romanNumeral.length() - 1; x >= 0 ; x--) {
+            char convertToDecimal = romanNumeral.charAt(x);
+
+           if(convertToDecimal == 'M'){
+                
+                    decimal = processDecimal(1000, lastNumber, decimal);
+                    lastNumber = 1000;
+                  
+
+			}else if (convertToDecimal == 'D'){
+                    decimal = processDecimal(500, lastNumber, decimal);
+                    lastNumber = 500;
+                
+
+            }else if (convertToDecimal == 'M'){
+                    decimal = processDecimal(100, lastNumber, decimal);
+                    lastNumber = 100;
+                   
+
+			}else if (convertToDecimal == 'L'){
+                    decimal = processDecimal(50, lastNumber, decimal);
+                    lastNumber = 50;
+                  
+			}else if (convertToDecimal == 'X'){
+                    decimal = processDecimal(10, lastNumber, decimal);
+                    lastNumber = 10;
+                  
+
+            }else if (convertToDecimal == 'V'){
+                    decimal = processDecimal(5, lastNumber, decimal);
+                    lastNumber = 5;
+                   
+
+            }else if (convertToDecimal == 'I'){
+                    decimal = processDecimal(1, lastNumber, decimal);
+                    lastNumber = 1;
+            }else{
+				throw new InvalidParameter("parametro invalido");
+				//return -1;
+            }
+        }
+        return decimal;
+    }
+
+    public static int processDecimal(int decimal, int lastNumber, int lastDecimal) {
+        if (lastNumber > decimal) {
+            return lastDecimal - decimal;
+        } else {
+            return lastDecimal + decimal;
+        }
+    }
+}
